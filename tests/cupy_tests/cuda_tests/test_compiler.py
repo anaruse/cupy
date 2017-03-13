384ef905d2bf9aa6a5a64a3237b82d808cc3703d
Shunta Saito <shunta.saito@gmail.com>
2017-03-13 17:50:02 +0900

    Use NVRTC

--- tests/cupy_tests/cuda_tests/test_compiler.py
+++ tests/cupy_tests/cuda_tests/test_compiler.py
@@ -2,35 +2,15 @@ import os
 import unittest
 
 import six
-
-from cupy.cuda import compiler
 from cupy import testing
+from cupy.cuda import compiler
+
+from pynvrtc.compiler import ProgramException
 
 
 @testing.gpu
-class NvccNotFoundTest(unittest.TestCase):
-
-    def setUp(self):
-        self.path = os.environ.pop('PATH', '')
-
-    def tearDown(self):
-        os.environ['PATH'] = self.path
-
-    def test_nvcc_without_command(self):
-        # Check that error message includes command name `nvcc`
-        with six.assertRaisesRegex(self, OSError, 'nvcc'):
-            compiler.nvcc('')
-
-    def test_preprocess_without_command(self):
-        # Check that error message includes command name `nvcc`
-        with six.assertRaisesRegex(self, OSError, 'nvcc'):
-            compiler.preprocess('')
-
-
-@testing.gpu
-class TestNvccStderr(unittest.TestCase):
+class TestNvrtcErr(unittest.TestCase):
 
     def test(self):
-        # An error message contains the file name `kern.cu`
-        with six.assertRaisesRegex(self, RuntimeError, 'kern.cu'):
-            compiler.nvcc('a')
+        with self.assertRaises(ProgramException):
+            compiler.nvrtc('a')
