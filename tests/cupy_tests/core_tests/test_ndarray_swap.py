2ba88c23de8e0b3646f08fecf49c75a0d652defc
anaruse <anaruse@nvidia.com>
2017-06-05 18:46:32 +0900

    Add copy routines to pinned memory class

--- tests/cupy_tests/core_tests/test_ndarray_swap.py
+++ tests/cupy_tests/core_tests/test_ndarray_swap.py
@@ -4,31 +4,54 @@ import cupy
 from cupy import cuda
 from cupy import testing
 
+import copy
+
 
 @testing.gpu
 class TestArraySwap(unittest.TestCase):
 
-    _multiprocess_can_split_ = True
     shape = (2, 3, 4)
 
     def setUp(self):
-        self.stream = cuda.Stream.null
+        self.stream = cuda.Stream()
 
     @testing.for_all_dtypes()
     def test_swapout(self, dtype):
-        a = core.ndarray(self.shape)
-        a.fill(1)
+        a = cupy.random.uniform(0, 2, self.shape).astype(dtype)
         a.swapout()
-        testing.assertTrue(a.is_swapout)
-        testing.assertEqual(a.data, None)
+        self.assertTrue(a.is_swapout)
+        self.assertEqual(a.data, None)
+        self.assertNotEqual(a.data_swapout, None)
 
+    @testing.for_all_dtypes()
+    def test_swapout_async(self, dtype):
+        a = cupy.random.uniform(0, 2, self.shape).astype(dtype)
+        a.swapout(stream=self.stream)
+        self.stream.synchronize()
+        self.assertTrue(a.is_swapout)
+        self.assertEqual(a.data, None)
+        self.assertNotEqual(a.data_swapout, None)
 
     @testing.for_all_dtypes()
     def test_swapin(self, dtype):
-        a = core.ndarray(self.shape)
-        a.fill(1)
+        a = cupy.random.uniform(0, 2, self.shape).astype(dtype)
+        b = copy.deepcopy(a)
         a.swapout()
         a.swapin()
-        testing.assertFalse(a.is_swapout)
-        testing.assertEqual(a.data_swapout, None)
+        self.assertFalse(a.is_swapout)
+        self.assertNotEqual(a.data, None)
+        self.assertEqual(a.data_swapout, None)
+        testing.assert_array_equal(a, b)
+
+    @testing.for_all_dtypes()
+    def test_swapin_async(self, dtype):
+        a = cupy.random.uniform(0, 2, self.shape).astype(dtype)
+        b = copy.deepcopy(a)
+        a.swapout(stream=self.stream)
+        a.swapin(stream=self.stream)
+        self.stream.synchronize()
+        self.assertFalse(a.is_swapout)
+        self.assertNotEqual(a.data, None)
+        self.assertEqual(a.data_swapout, None)
+        testing.assert_array_equal(a, b)
 
